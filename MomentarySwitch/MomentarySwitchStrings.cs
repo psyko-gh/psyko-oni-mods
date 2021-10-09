9136fcc216abab027843f6c0c93b9834e912f103
psyko-gh <psykogithub@gmail.com>
2021-10-09 23:50:26 +0200

    Fixed some inverted strings

--- MomentarySwitch/MomentarySwitchStrings.cs
+++ MomentarySwitch/MomentarySwitchStrings.cs
@@ -11,8 +11,8 @@ namespace Psyko.MomentarySwitch
                 public class MOMENTARYSWITCH
                 {
                     public static LocString NAME = FormatAsLink("Momentary switch", MomentarySwitchConfig.ID);
-                    public static LocString DESC = "Send a logic pulse before going back to its initial state";
-                    public static LocString EFFECT = "Signal switches don't turn grids on and off like power switches, but add an extra signal.";
+                    public static LocString DESC = "Signal switches don't turn grids on and off like power switches, but add an extra signal.";
+                    public static LocString EFFECT = "Send a logic pulse before going back to its initial state";
                     public static LocString LOGIC_PORT = "Signal output";
                     public static LocString LOGIC_PORT_ACTIVE = "Sends a " + FormatAsAutomationState("Green Signal", AutomationState.Active) + " when pressed, for a small duration";
                     public static LocString LOGIC_PORT_INACTIVE = "Otherwise, sends a " + FormatAsAutomationState("Red Signal", AutomationState.Standby);
